b784a5cf4035db9837b54e5c28a4a3ba662ac39f
zhansayasovetova <zhansaya.sovetova@narxoz.kz>
2026-04-05 22:28:07 +0500

    Implement HpHandler to apply final damage to health

--- src/com/narxoz/rpg/chain/HpHandler.java
+++ src/com/narxoz/rpg/chain/HpHandler.java
@@ -6,9 +6,11 @@ public class HpHandler extends DefenseHandler {
 
     @Override
     public void handle(int incomingDamage, ArenaFighter target) {
-        // TODO: Apply incomingDamage to the target using target.takeDamage(int).
-        // TODO: Print a message showing how much damage landed.
-        // Note: This is the terminal handler — never call passToNext here.
-        //       If damage reaches this point, it hits the fighter's HP, no matter what.
+        target.takeDamage(incomingDamage);
+
+        System.out.println(target.getName() + " took "
+                + incomingDamage + " damage");
+
+
     }
 }
